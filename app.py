d3b58680d9a1efade1820268f25152bfc2532309
Nathanuel Dixon <nathan0322@hotmail.com>
2024-03-01 02:07:46 -0500

    changes - import dotenv to get db user and pw from .env - added base code for mongodb server in app.py - added routes for edit and delete in app.py - changes button now fully operational and redirects to upload page - Side tabs embolden on the page they direct to

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, jsonify, render_template
 from flask import redirect
-# from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PW = os.getenv("DB_PW")
 
 app = Flask(__name__)
 
+uri = f"mongodb+srv://{DB_USER}:{DB_PW}@sweproject2.v6vtrh6.mongodb.net/?retryWrites=true&w=majority&appName=SWEProject2"
 
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 '''
 CREATE TEMPLATES AND RENDER THEM DEPENDING ON THE ENDPOINT
@@ -31,6 +50,14 @@ def taxes():
 @app.route('/insights')
 def insights():
     return render_template('insights.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
 
 
 if __name__ == '__main__':
